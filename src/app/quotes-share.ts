207aa9846aa5406194343e19521d8e42d79f8f81
Mary Waithera <njihiamary11@gmail.com>
2020-03-27 16:41:24 +0300

    Create sample quotes files and use the quotesshare interface to create quotes in this file, Use iteractive directive and interpolation data binding to display the titles of created quotes, and use bootstrap and css to style the displayed titles and create hover effect

--- src/app/quotes-share.ts
+++ src/app/quotes-share.ts
@@ -1,2 +1,7 @@
 export interface QuotesShare {
+  title: string;
+  quote: string;
+  author: string;
+  dateCreated: Date;
+  addedBy: string;
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { QuotesShare } from '../quotes-share';
+import { QUOTES } from '../existing-quotes';
+
 
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
+
 export class QuotesComponent implements OnInit {
+
+  quotes=QUOTES;
 
   constructor() { }
 
