7dd5d6cdcb51ba520d6a9a274b7a4e2e707309fe
Mary Waithera <njihiamary11@gmail.com>
2020-03-29 19:02:37 +0300

    Use ngif directive to ensure that quote details are only visible if user clicks on view quote button and change font-family to enhance the legibility of the text

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -15,7 +15,7 @@ export class QuotesComponent implements OnInit {
     new SampleQuotes('food', 'One cannot think well, love well, sleep well, if one has not dined well', 'Virginia Woolf', new Date(2020, 2, 27), 'Njihia',),
     new SampleQuotes('hate', 'Hating people is like burning down your own house to get rid of a rat', 'Henry Emerson Fosdick', new Date(2020, 2, 27), 'May'),
     new SampleQuotes('equality', 'I hate to hear you talk about all women as if they were fine ladies instead of rational creatures. None of us want to be in calm waters all our lives', 'Jane Austen', new Date(2020, 2, 27), 'Njihia'),
-    new SampleQuotes('dreams', 'dreams are representations of what we wish and fear and offer useful insights into what out lives would be if we learned to live without inhibitions', 'Mary Njihia', new Date(2020, 2, 27), 'Njihia'),
+    new SampleQuotes('dreams', 'Dreams are representations of what we wish and fear and offer useful insights into what out lives would be if we learned to live without inhibitions', 'Mary Njihia', new Date(2020, 2, 27), 'Njihia'),
   ];
  
   
