ee92ca7613fd545d04bcb16960781cc0a55f7064
Mary Waithera <njihiamary11@gmail.com>
2020-07-31 14:07:00 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuoteContentComponent } from './quote-content/quote-content.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: QuoteContentComponent},
+  {path: 'details', component: QuoteDetailsComponent},
+  {path: 'add', component: QuotesFormComponent},
+  {path: 'view', component: QuotesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
